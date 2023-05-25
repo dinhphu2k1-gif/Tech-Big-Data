f0c9c2b778ddf744fdfb4b6da37db722e3685987
dinhphu2k1-gif <dinhphu012001@gmail.com>
2023-05-25 22:00:14 +0700

    add spark cluster

---
+++ spark/docker/config-spark/spark-env.sh
@@ -0,0 +1,7 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 
+export SPARK_HOME=/usr/local/spark
+export HADOOP_HOME=/usr/local/hadoop 
+
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_LOG_DIR=$SPARK_HOME/logs
